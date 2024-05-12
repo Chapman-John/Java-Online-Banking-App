0fd8d808589bab6f5f8fa413a385472bd7f19bdc
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-05-12 16:53:33 -0700

    update account.java

--- online-banking/src/main/java/com/bankapp/onlinebanking/entity/Account.java
+++ online-banking/src/main/java/com/bankapp/onlinebanking/entity/Account.java
@@ -1,6 +1,7 @@
 package com.bankapp.onlinebanking.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "accounts")
 @Entity
 public class Account {
+
+    @Id
     private Long id;
     private String accountHolderName;
     private String balance;
