09d48cafdef02731875ab0a0db6895f1cb8e4b89
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-06-27 16:38:31 -0700

    update account repository

--- online-banking/src/main/java/com/bankapp/onlinebanking/repository/AccountRepository.java
+++ online-banking/src/main/java/com/bankapp/onlinebanking/repository/AccountRepository.java
@@ -1,5 +1,10 @@
 package com.bankapp.onlinebanking.repository;
+// package net.javaguides.banking.repository;
 
-public interface AccountRepository {
+// import org.springframework.stereotype.Repository;
+import com.bankapp.onlinebanking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
