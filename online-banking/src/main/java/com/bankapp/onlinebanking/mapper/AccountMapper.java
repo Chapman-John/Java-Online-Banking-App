b6f7025997c09282d95e2cc46537ac50bf4e1754
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-08-28 13:29:02 -0700

    more updates for mysql

--- online-banking/src/main/java/com/bankapp/onlinebanking/mapper/AccountMapper.java
+++ online-banking/src/main/java/com/bankapp/onlinebanking/mapper/AccountMapper.java
@@ -1,27 +1,27 @@
-package com.bankapp.onlinebanking.mapper;
+// package com.bankapp.onlinebanking.mapper;
 
-import com.bankapp.onlinebanking.entity.Account;
-import com.bankapp.onlinebanking.dto.AccountDto;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import com.bankapp.onlinebanking.entity.Account;
+// import com.bankapp.onlinebanking.dto.AccountDto;
+// import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-public class AccountMapper {
+// public class AccountMapper {
 
-    // public Account mapToAccount(AccountDto accountDto) {
-    // Account account = new Account(
-    // accountDto.getId(),
-    // accountDto.getAccountHolderName(),
-    // accountDto.getBalance());
-    // return account;
-    // }
+// public Account mapToAccount(AccountDto accountDto) {
+// Account account = new Account(
+// accountDto.getId(),
+// accountDto.getAccountHolderName(),
+// accountDto.getBalance());
+// return account;
+// }
 
-    // public AccountDto mapToAccountDto(Account account) {
-    // AccountDto accountDto = new AccountDto(
-    // account.getId(),
-    // account.getAccountHolderName(),
-    // account.getBalance());
-    // return accountDto;
-    // }
+// public AccountDto mapToAccountDto(Account account) {
+// AccountDto accountDto = new AccountDto(
+// account.getId(),
+// account.getAccountHolderName(),
+// account.getBalance());
+// return accountDto;
+// }
 
-}
+// }
--- online-banking/src/main/java/com/bankapp/onlinebanking/service/impl/AccountServiceImpl.java
+++ online-banking/src/main/java/com/bankapp/onlinebanking/service/impl/AccountServiceImpl.java
@@ -1,13 +1,13 @@
-package com.bankapp.onlinebanking.service.impl;
+// package com.bankapp.onlinebanking.service.impl;
 
-import com.bankapp.onlinebanking.service.AccountService;
-import com.bankapp.onlinebanking.dto.AccountDto;
-import com.bankapp.onlinebanking.mapper.AccountMapper;
-import com.bankapp.onlinebanking.repository.AccountRepository;
-import org.springframework.stereotype.Service;
-import com.bankapp.onlinebanking.entity.Account;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.stereotype.Component;
+// import com.bankapp.onlinebanking.service.AccountService;
+// import com.bankapp.onlinebanking.dto.AccountDto;
+// import com.bankapp.onlinebanking.mapper.AccountMapper;
+// import com.bankapp.onlinebanking.repository.AccountRepository;
+// import org.springframework.stereotype.Service;
+// import com.bankapp.onlinebanking.entity.Account;
+// import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.stereotype.Component;
 
 // @Service
 // public class AccountServiceImpl implements AccountService {
