0badea9b81ee93ea5b7c5d1bf297e112a5ffd890
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-11-18 13:45:19 -0800

    mvc update

--- online-banking/src/main/java/com/bankapp/onlinebanking/entity/Account.java
+++ online-banking/src/main/java/com/bankapp/onlinebanking/entity/Account.java
@@ -3,25 +3,31 @@ package com.bankapp.onlinebanking.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
+// import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+// @Getter
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
 @Entity
 @Table(name = "accounts")
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private Long id;
 
     // @Column(name = "account_holder_name", nullable = false, length = 100)
+    @Getter
+    @Setter
     private String accountHolderName;
 
     // @Column(nullable = false)
+    // @Getter
+    // @Setter
     private double balance = 0.0;
     // private double balance;
 
